6248ec90588350c1c7f17e356dffd01d7d507524
mayaj0yce <m.j.thayne@gmail.com>
2023-09-29 23:48:58 -0600

    get and post and delete are all working

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,92 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+//find all tags
+router.get('/', async (req, res) => {
+  try {
+    const tagData = await Tag.findAll({
+      include: [Product],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [Product],
+    });
+
+    if (!tagData) {
+      res
+        .status(400)
+        .json({ message: "No catergory tag fround with that id!" });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/', async (req, res) => {
+  try {
+    const tagData = await Tag.create({
+      tag_id: req.body.tag_id,
+      tag_name: req.body.tag_name,
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const [updatedRowCount] = await Tag.update(
+      { tag_name: req.body.tag_name },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+
+    if (updatedRowCount === 0) {
+      res.status(404).json({ message: "No tag found with that id!" });
+      return;
+    }
+
+    const updatedTag = await Tag.findByPk(req.params.id, {
+      include: [Product],
+    });
+
+    res.status(200).json(updatedTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: "No tag found with that id!" });
+      return;
+    }
+
+    res.status(200).json("Tag has been deleted.");
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const routes = require('./routes');
+// const sequelize = require('sequelize');
 // import sequelize connection
+ const sequelize = require('./config/connection');
 
+ 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -11,6 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
+sequelize.sync().then(() => {
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
+})
 });
